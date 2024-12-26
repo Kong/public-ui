7383d88ec0dd6c14c2e4c07284a6de4a40a85431
Tian Tan <tian.tan@konghq.com>
2024-12-26 14:13:04 +0800

    feat(plugin-form): data cache during partial toggle

--- packages/entities/entities-plugins/src/components/PluginEntityForm.vue
+++ packages/entities/entities-plugins/src/components/PluginEntityForm.vue
@@ -17,6 +17,7 @@
         :form-schema="formSchema"
         :is-editing="editing"
         :on-model-updated="onModelUpdated"
+        :on-partial-toggled="onPartialToggled"
       >
         <template
           v-if="enableVaultSecretPicker"
@@ -36,6 +37,7 @@
         :options="formOptions"
         :schema="formSchema"
         @model-updated="onModelUpdated"
+        @partial-toggled="onPartialToggled"
         @refresh-model="getModel"
       >
         <template #plugin-config-empty-state>
@@ -181,7 +183,7 @@ const { parseSchema } = composables.useSchemas({
   entityId: props.entityMap.focusedEntity?.id || undefined,
   credential: props.credential,
 })
-const { convertToDotNotation, unFlattenObject, isObjectEmpty, unsetNullForeignKey } = composables.usePluginHelpers()
+const { convertToDotNotation, unFlattenObject, dismissField, isObjectEmpty, unsetNullForeignKey } = composables.usePluginHelpers()
 
 const { objectsAreEqual } = useHelpers()
 const { i18n: { t } } = useI18n()
@@ -511,6 +513,15 @@ const getModel = (): Record<string, any> => {
   return unFlattenObject(outputModel)
 }
 
+const onPartialToggled = (dismissSchemaField: string | undefined, additionalModel: Record<string, any> = {}) => {
+  dismissField(formModel, additionalModel, dismissSchemaField)
+  emit('model-updated', {
+    model: formModel,
+    originalModel,
+    data: getModel(),
+  })
+}
+
 // fired whenever the form data is modified
 const onModelUpdated = (model: any, schema: string) => {
   const newData = { [schema]: model }
@@ -520,7 +531,6 @@ const onModelUpdated = (model: any, schema: string) => {
   const newModel = Object.assign({}, formModel, newData)
 
   Object.assign(formModel, newModel)
-
   emit('model-updated', {
     model: formModel,
     originalModel,
